239ce298cb2284b2686a5a23f22cf7dc625f7be3
Lazaro H. Marques <lazarohcm@gmail.com>
2013-10-09 11:32:55 -0300

    Carregando imagens

---
+++ gunsdontkillpeople/gunsdontkillpeople/Carrega/Imagens.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace gunsdontkillpeople
+{
+    class Imagens
+    {
+        public Texture2D player;
+
+        public void Load(ContentManager Content)
+        {
+            player = Content.Load<Texture2D>(@"assets/player/player");
+        }
+    }
+}
